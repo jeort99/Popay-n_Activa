5436cd00433367ca91c628dbac7f51fbc791ba17
Jose99420 <jeort420@gmail.com>
2026-05-10 16:18:55 -0500

    Database fixed

--- popayanactiva/settings.py
+++ popayanactiva/settings.py
@@ -113,10 +113,11 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+""" 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
+"""
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ==========================================
