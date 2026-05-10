679f17b7b26bdc61530ec8ed3f460c0c55344fb2
Jose99420 <jeort420@gmail.com>
2026-05-10 16:33:49 -0500

    Use PostgresSQL

--- popayanactiva/settings.py
+++ popayanactiva/settings.py
@@ -73,8 +73,9 @@ WSGI_APPLICATION = 'popayanactiva.wsgi.application'
 # ==========================================
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
